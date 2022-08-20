b5158ec8d668e01c9baa4b71ad6979845c178110
Rifat Halim <rhalim@magicleap.com>
2022-08-20 10:53:33 -0400

    Created a separate class for SQL data driven testing

--- src/test/java/automation_test/mortgage_calculator/CalculateMortgageRate.java
+++ src/test/java/automation_test/mortgage_calculator/CalculateMortgageRate.java
@@ -4,37 +4,27 @@ import automation_test.BaseUIClass;
 import org.testng.annotations.Test;
 import page_objects.Home;
 import utilities.DateUtils;
-import utilities.SqlConnector;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class CalculateMortgageRate extends BaseUIClass {
     @Test
     public void calculateMonthlyPayment() {
         String[] date = DateUtils.returnNextMonth();
-        try {
-            ResultSet rs = SqlConnector.readData("select * from monthly_mortgage");
-            while (rs.next()) {
-                new Home(driver)
-                        .typeHomePrice(rs.getString("homevalue"))
-                        .clickDownPaymentInDollar()
-                        .typeDownPayment(rs.getString("downpayment"))
-                        .typeLoanAmount(rs.getString("loanamount"))
-                        .typeInterestRate(rs.getString("interestrate"))
-                        .typeLoanTermYears(rs.getString("loanterm"))
-                        .selectMonth(date[0])
-                        .typeYear(date[1])
-                        .typePropertyTax(rs.getString("propertytax"))
-                        .typePMI(rs.getString("pmi"))
-                        .typeHomeOwnerInsurance(rs.getString("homeownerinsurance"))
-                        .typeMonthlyHOA(rs.getString("monthlyhoa"))
-                        .selectLoanType(rs.getString("loantype"))
-                        .selectBuyOrRefinance(rs.getString("buyorrefi"))
-                        .clickOnCalculateButton()
-                        .validateTotalMonthlyPayment(rs.getString("totalmonthlypayment"));
-            }
-        } catch (SQLException e) {
-            LOGGER.error("SQL Data Read Exception: " + e.getMessage());
-        }
+        new Home(driver)
+                .typeHomePrice("300000")
+                .clickDownPaymentInDollar()
+                .typeDownPayment("60000")
+                .typeLoanAmount("240000")
+                .typeInterestRate("3")
+                .typeLoanTermYears("30")
+                .selectMonth(date[0])
+                .typeYear(date[1])
+                .typePropertyTax("5000")
+                .typePMI("0.5")
+                .typeHomeOwnerInsurance("1000")
+                .typeMonthlyHOA("100")
+                .selectLoanType("FHA")
+                .selectBuyOrRefinance("Buy")
+                .clickOnCalculateButton()
+                .validateTotalMonthlyPayment("1,611.85");
     }
 }
